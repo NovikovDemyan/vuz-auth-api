19052b804cd28f060461801dd7c3260d29de2ad9
NovikovDemyan <novikov.di@phystech.edu>
2025-12-08 23:01:43 +0500

    Fix: Added Netlify domain to CORS allowedOrigins

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const usersDB = [
 // --- НАСТРОЙКА CORS (Критично для работы на разных доменах) ---
 // ВНИМАНИЕ: Замените заглушку на реальный публичный домен вашего фронтенда (например, https://vuz-portal.vercel.app)
 const allowedOrigins = [
+    'https://fanciful-gingersnap-d87b1c.netlify.app',
     'http://localhost:5500', // Для локальной работы с Live Server
     'http://localhost:3000', // Если фронтенд запущен здесь же
     'https://fanciful-gingersnap-d87b1c.netlify.app/' // <<< ВСТАВЬТЕ СЮДА АДРЕС САЙТА ВУЗА
